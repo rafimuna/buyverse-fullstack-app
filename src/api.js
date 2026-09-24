2128d43c1472086fa20f04c305b49623a014fa0c
rafimuna <rafimuna77@gmail.com>
2026-09-24 19:58:26 +0600

    Chore: Configure frontend env and netlify redirects

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
-  
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api/',
 });
 
 // যেসব API এন্ডপয়েন্টে Authorization হেডার পাঠানো যাবে না (Public Endpoints)
